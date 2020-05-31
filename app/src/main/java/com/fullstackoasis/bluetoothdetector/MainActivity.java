735b395fdb4ca9ea14e0fd43d0564d59ae2e779c
fullStackOasis <fullstackdev@fullstackoasis.com>
2020-05-31 11:34:28 -0400

    Check whether Bluetooth is or is not available, and show appropriate messages for each case. Tested on an Emulator that does not have Bluetooth and on a device that does. Works as expected.

--- app/src/main/java/com/fullstackoasis/bluetoothdetector/MainActivity.java
+++ app/src/main/java/com/fullstackoasis/bluetoothdetector/MainActivity.java
@@ -2,7 +2,9 @@ package com.fullstackoasis.bluetoothdetector;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
+import android.widget.TextView;
 
 /**
  * This simple app is a Bluetooth detector.
@@ -10,10 +12,55 @@ import android.os.Bundle;
  * This app will use classic Bluetooth.
  */
 public class MainActivity extends AppCompatActivity {
+    private boolean bluetoothNotSupported = true;
 
+    /**********************************************************************************************
+     * Lifecycle methods
+     **********************************************************************************************/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getBluetoothAdapter();
+    }
+
+    /**********************************************************************************************
+     * Bluetooth handling methods
+     **********************************************************************************************/
+
+    /**
+     * Check to see if Bluetooth is supported.
+     */
+    private void getBluetoothAdapter() {
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            // Device doesn't support Bluetooth
+            handleBluetoothNotSupported();
+            return;
+        }
+        bluetoothNotSupported = false;
+        handleBluetoothIsSupported();
+    }
+
+    /**
+     * Handle Bluetooth not being supported by showing appropriate message to user.
+     */
+    private void handleBluetoothNotSupported() {
+        TextView tv = findViewById(R.id.bluetooth_status);
+        tv.setText(R.string.bluetooth_unavailable);
+    }
+
+    /**
+     * Handle Bluetooth being supported by showing appropriate message to user.
+     */
+    private void handleBluetoothIsSupported() {
+        TextView tv = findViewById(R.id.bluetooth_status);
+        tv.setText(R.string.bluetooth_available);
+    }
+
 }
